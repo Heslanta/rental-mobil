3a7342dd85e85bb7e347ea5eea255ded924afa2b
Charvia <90585985+Heslanta@users.noreply.github.com>
2023-09-09 23:20:11 +0700

    ubah title

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -5,7 +5,6 @@ function Topbar() {
   return (
     <nav className="topbar">
       <Link href="/" className="flex item-center gap-3">
-        <Image src="/logo.svg" alt="logo" height={28} width={28} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden ">
           E-Rental
         </p>

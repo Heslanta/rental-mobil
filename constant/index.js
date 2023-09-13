8882d9e0134b98ac724c0eedded5206258568d2b
Charvia <90585985+Heslanta@users.noreply.github.com>
2023-09-13 19:28:46 +0700

    add constant for the web

---
+++ constant/index.js
@@ -0,0 +1,47 @@
+// untuk menambahkan constant pada websitenya sehingga tidak perlu untuk mengulang beberapa kali
+// dan lebih mudah untuk menambahkan disini
+
+export const navbarLinks = [
+  {
+    imgURL: "/assets/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/about.svg",
+    route: "/about",
+    label: "About",
+  },
+  {
+    imgURL: "/assets/car.svg",
+    route: "/car",
+    label: "Car",
+  },
+  {
+    imgURL: "/assets/contact.svg",
+    route: "/contact",
+    label: "Contact",
+  },
+];
+export const sidebarLinks = [
+  {
+    imgURL: "/assets/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/about.svg",
+    route: "/",
+    label: "About",
+  },
+  {
+    imgURL: "/assets/car.svg",
+    route: "/",
+    label: "Car",
+  },
+  {
+    imgURL: "/assets/contact.svg",
+    route: "/",
+    label: "Contact",
+  },
+];

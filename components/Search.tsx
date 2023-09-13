218b9434fdfb696f18496917eebfc599cd7e004c
Charvia <90585985+Heslanta@users.noreply.github.com>
2023-09-13 19:28:34 +0700

    add search tsx

---
+++ components/Search.tsx
@@ -0,0 +1,12 @@
+function Search() {
+  return (
+    <div className="h-screen bg-dark-1 ">
+      <div className="text-light-1 flex-col">
+        <h1 className="text-heading1-semibold items-center ">Search</h1>
+        <p>ADADADADA</p>
+      </div>
+    </div>
+  );
+}
+
+export default Search;

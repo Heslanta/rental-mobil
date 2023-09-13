ccfe35d333b8e3f806ee5bf1a0cbc5d57c4c2581
Charvia <90585985+Heslanta@users.noreply.github.com>
2023-09-13 19:27:38 +0700

    add about

---
+++ components/About.tsx
@@ -0,0 +1,56 @@
+function About() {
+  return (
+    <div className="h-screen bg-dark-1 text-center">
+      <div className="text-slate-200 p-0">
+        <div className="cards text-center flex flex-wrap justify-between px-16 ">
+          <div className=" w-80  bg-dark-1 border  rounded-lg shadow border-white border-1 border-solid">
+            <div className="p-3">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight dark:text-white ">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p className="font-normal ">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </div>
+          </div>
+          <div className=" w-80  bg-dark-1 border  rounded-lg shadow border-white border-1 border-solid">
+            <div className="p-3">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight dark:text-white ">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p className="font-normal ">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </div>
+          </div>
+          <div className=" w-80  bg-dark-1 border  rounded-lg shadow border-white border-1 border-solid">
+            <div className="p-3">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight dark:text-white ">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p className="font-normal ">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </div>
+          </div>
+          <div className=" w-80  bg-dark-1 border  rounded-lg shadow border-white border-1 border-solid">
+            <div className="p-3">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight dark:text-white ">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p className="font-normal ">
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
